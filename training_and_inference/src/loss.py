0c7ddee65dc45819301333bd3b1386dcb91a9cfd
Moeberg <bgr549@alumni.ku.dk>
2025-06-04 22:53:48 +0200

    Changed loss function to MAE

--- training_and_inference/src/loss.py
+++ training_and_inference/src/loss.py
@@ -9,12 +9,21 @@ import torch.nn.functional as F
 #     loss = torch.mean((y_pred - target) ** 2)
 #     return loss
 
-def MSE_loss(y_pred, target, delta=0.1e6):
+# MAE Loss
+def MSE_loss(y_pred, target):
     y_pred = y_pred.squeeze()
     target = target.squeeze()
     
-    loss = F.huber_loss(y_pred, target, delta=delta, reduction='mean')
+    loss = torch.mean(torch.abs(y_pred - target))
     return loss
+
+# Huber Loss
+# def MSE_loss(y_pred, target, delta=0.1e6):
+#     y_pred = y_pred.squeeze()
+#     target = target.squeeze()
+    
+#     loss = F.huber_loss(y_pred, target, delta=delta, reduction='mean')
+#     return loss
 
 
 
